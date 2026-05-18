b1191704479ce0a514bfdc6b2743898b0a11cda6
TaurusGGBOY <guobingaogg@gmail.com>
2026-05-18 22:43:25 +0800

    feat: send custom toast xml through powershell

--- windows-server/go.mod
+++ windows-server/go.mod
@@ -2,20 +2,16 @@ module agent-notify-server
 
 go 1.25.0
 
-require github.com/cenkalti/backoff v2.2.1+incompatible
+require github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 
 require github.com/grandcat/zeroconf v1.0.0
 
-require github.com/miekg/dns v1.1.27
+require github.com/miekg/dns v1.1.27 // indirect
 
-require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+require golang.org/x/crypto v0.50.0 // indirect
 
-require github.com/go-toast/toast v0.0.0-20190205052223-0c4b349fca45
+require golang.org/x/net v0.53.0 // indirect
 
-require golang.org/x/crypto v0.50.0
+require golang.org/x/sys v0.43.0 // indirect
 
-require golang.org/x/net v0.53.0
-
-require golang.org/x/sys v0.43.0
-
-require golang.org/x/image v0.40.0 // indirect
+require golang.org/x/image v0.40.0
--- windows-server/toast_windows.go
+++ windows-server/toast_windows.go
@@ -4,12 +4,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
-
-	"github.com/go-toast/toast"
+	"os/exec"
 )
 
-// ToastNotifier sends Windows toast notifications using go-toast library
 type ToastNotifier struct {
 	appName string
 }
@@ -18,43 +17,61 @@ func NewToastNotifier(appName string) *ToastNotifier {
 	return &ToastNotifier{appName: appName}
 }
 
-// Notify sends a basic notification (legacy method)
 func (n *ToastNotifier) Notify(title, message string) error {
-	notification := toast.Notification{
-		AppID:   n.appName,
-		Title:   title,
-		Message: message,
-	}
-	return notification.Push()
+	return n.NotifyWithStyle("clean", "stop", title, message, "")
 }
 
-// NotifyWithStyle sends a notification with style-based customization
-// Note: go-toast has limited customization. Styles are approximated via title/message formatting.
 func (n *ToastNotifier) NotifyWithStyle(style, event, title, message, agent string) error {
-	notification := toast.Notification{
-		AppID:   n.appName,
-		Title:   title,
-		Message: message,
+	project := ""
+	cardPath := ""
+	if style == "custom-card" {
+		path, err := toastCardPath()
+		if err != nil {
+			return err
+		}
+		card := ToastCard{
+			Event:   event,
+			Title:   title,
+			Agent:   agent,
+			Project: project,
+			Message: message,
+		}
+		if err := renderToastCard(path, card); err != nil {
+			return err
+		}
+		cardPath = path
 	}
-
-	// Style-specific adjustments via go-toast library capabilities
-	switch style {
-	case "status-color":
-		// Status color uses attribution text feature if available
-		notification.Audio = toast.Default
-	case "agent-badge":
-		// Agent badge uses custom icon if available
-		notification.Audio = toast.Default
-	case "compact":
-		// Compact uses no sound
-		notification.Audio = toast.Silent
-	default:
-		notification.Audio = toast.Default
-	}
-
-	if err := notification.Push(); err != nil {
+	xml := formatToastXML(style, event, title, agent, project, cardPath)
+	if err := sendToastViaPowerShell(n.appName, xml); err != nil {
 		log.Printf("Toast notification failed: %v", err)
 		return err
 	}
 	return nil
+}
+
+func sendToastViaPowerShell(appID, xml string) error {
+	encodedXML := base64.StdEncoding.EncodeToString([]byte(xml))
+	script := `
+$xml = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('` + encodedXML + `'))
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+$doc = [Windows.Data.Xml.Dom.XmlDocument]::new()
+$doc.LoadXml($xml)
+$toast = [Windows.UI.Notifications.ToastNotification]::new($doc)
+[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('` + escapePowerShellSingleQuoted(appID) + `').Show($toast)
+`
+	cmd := exec.Command("powershell.exe", "-NoProfile", "-ExecutionPolicy", "Bypass", "-WindowStyle", "Hidden", "-Command", script)
+	return cmd.Run()
+}
+
+func escapePowerShellSingleQuoted(s string) string {
+	out := ""
+	for _, r := range s {
+		if r == '\'' {
+			out += "''"
+		} else {
+			out += string(r)
+		}
+	}
+	return out
 }
